0fd50c438917d3f73b2b34d2422a1b83d2bcaaa4
yusuke-tanaka-ew <yusuke.tanaka.keio@gmail.com>
2021-02-21 23:04:13 +0900

    COMPLETE ~P106

--- sample-redux/src/index.js
+++ sample-redux/src/index.js
@@ -1,54 +1,85 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createStore} from 'redux'
+// import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
 
 
 const initialState = {
-  tasks:[]
+  task:'defalut task',
+  tasks : ['default task']
 };
 
-function taskReducer(state = initialState,action){
-  switch(action.type){
-    case 'ADD_TASK' :
+
+function taskReducer(state= initialState,action){
+  switch (action.type){
+    case 'INPUT_TASK':
       return {
         ...state,
-        tasks: state.tasks.concat([action.payload.task])
-      }
-    default :
+        task : action.payload.task
+      };
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks : state.tasks.concat([action.payload.task])
+      };
+    default:
       return state;
   }
 }
 
-const addTask = (task) => ({
-  type: 'ADD_TASK',
+const store = createStore(taskReducer);
+
+const inputTask = (task) => ({
+  type : 'INPUT_TASK',
   payload : {
     task
   }
 });
 
-const store = createStore(taskReducer);
-
-function handleChange(){
-  console.log(store.getState());
-}
-
-const unsubscribe = store.subscribe(handleChange);
+const addTask = (task) => ({
+  type : 'ADD_TASK',
+  payload : {
+    task
+  }
+});
 
 console.log(store.getState());
 
-store.dispatch(addTask('learnig store'));
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function TodoApp({ store }) {
+  const { task, tasks } = store.getState();
+  return(
+    <div>
+      <input type='text' onChange={(e) => store.dispatch(inputTask(e.target.value))} />
+      <input type='button' value='ADD' onClick={()=>store.dispatch(addTask(task))} />
+      <ul>
+        {
+          tasks.map(function(item, i){
+            return(
+              <li key = {i}>{item}</li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+function renderApp(store){
+  render(
+    <TodoApp store = {store} />,
+    document.getElementById('root')
+  );
+}
+
+
+store.subscribe(() => renderApp(store));
+
+renderApp(store)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
