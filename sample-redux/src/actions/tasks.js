9eb75526559ec1563c2105cd75c0f026dc65e8a7
yusuke-tanaka-ew <yusuke.tanaka.keio@gmail.com>
2021-02-22 23:26:50 +0900

    Separate files   - reducer   - actins   - components

---
+++ sample-redux/src/actions/tasks.js
@@ -0,0 +1,17 @@
+
+
+
+export const inputTask = (task) => ({
+    type : 'INPUT_TASK',
+    payload : {
+        task
+    }
+});
+
+export const addTask = (task) => ({
+    type : 'ADD_TASK',
+    payload : {
+        task
+    }
+})
+
--- sample-redux/src/index.js
+++ sample-redux/src/index.js
@@ -1,70 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
-// import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import taskReducer from './reducers/tasks';
+import TodoApp from './components/TodoApp';
+import { createStore} from 'redux';
 
-
-const initialState = {
-  task:'defalut task',
-  tasks : ['default task']
-};
-
-
-function taskReducer(state= initialState,action){
-  switch (action.type){
-    case 'INPUT_TASK':
-      return {
-        ...state,
-        task : action.payload.task
-      };
-    case 'ADD_TASK':
-      return {
-        ...state,
-        tasks : state.tasks.concat([action.payload.task])
-      };
-    default:
-      return state;
-  }
-}
 
 const store = createStore(taskReducer);
-
-const inputTask = (task) => ({
-  type : 'INPUT_TASK',
-  payload : {
-    task
-  }
-});
-
-const addTask = (task) => ({
-  type : 'ADD_TASK',
-  payload : {
-    task
-  }
-});
-
-console.log(store.getState());
-
-
-function TodoApp({ store }) {
-  const { task, tasks } = store.getState();
-  return(
-    <div>
-      <input type='text' onChange={(e) => store.dispatch(inputTask(e.target.value))} />
-      <input type='button' value='ADD' onClick={()=>store.dispatch(addTask(task))} />
-      <ul>
-        {
-          tasks.map(function(item, i){
-            return(
-              <li key = {i}>{item}</li>
-            );
-          })
-        }
-      </ul>
-    </div>
-  );
-}
 
 function renderApp(store){
   render(
