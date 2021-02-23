9583dffa21ddb319f9d1e98d9a8dff33ea56dd83
yusuke-tanaka-ew <yusuke.tanaka.keio@gmail.com>
2021-02-23 23:58:02 +0900

    dd react-redux

---
+++ sample-react-redux/src/containers/TodoApp.js
@@ -0,0 +1,33 @@
+import { connect } from 'react-redux';
+import TodoApp from '../components/TodoApp';
+import { inputTask,addTask } from '../actions/tasks';
+
+
+/*
+function mapStateToProps({ task, tasks }){
+    return {
+        task,
+        tasks
+    };
+}
+*/
+
+
+const mapStateToProps = (state) => ({
+    task : state.task,
+    tasks : state.tasks,
+});
+
+
+function mapDispatchToProps(dispatch){
+    return {
+        addTask(task){
+            dispatch(addTask(task));
+        },
+        inputTask(task){
+            dispatch(inputTask(task))
+        }
+    };
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(TodoApp);
---
+++ sample-react-redux/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { provider } from 'react-redux';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import taskReducer from './reducers/tasks';
+import TodoApp from './containers/TodoApp';
+
+import reportWebVitals from './reportWebVitals';
+
+const store = createStore(taskReducer);
+
+render(
+  <provider store = {store}>
+    <TodoApp />
+  </provider>,
+  document.getElementById('root')
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
