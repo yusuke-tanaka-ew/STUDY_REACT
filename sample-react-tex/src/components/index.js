8005519272609a458b58852370c059b5d07d5fde
yusuke-tanaka-ew <yusuke.tanaka.keio@gmail.com>
2021-03-16 23:20:46 +0900

    ADD GRID SYSTEM

--- sample-react-tex/src/components/index.js
+++ sample-react-tex/src/components/index.js
@@ -3,7 +3,7 @@ import {InlineTex,Tex} from 'react-tex';
 
 import 'katex/dist/katex.min.css';
 
-import {TextareaAutosize} from '@material-ui/core'
+import {Grid,TextField} from '@material-ui/core'
 
 const latexString = "\int_{a}^{b} f(x)dx = F(b) - F(a)";
 const Latex = require('react-latex');
@@ -13,17 +13,24 @@ export default class TexArea extends React.Component{
     render(){
         return(
             <div className='texArea'>
-                <div className='inputArea'>
-                        <TextareaAutosize value = {this.props.texContent} onChange={(e)=>this.props.inputTex(e.target.value)} rowMin={3} />
-                </div>
-                <div className ='viewArea'>
-                    <h2>THIS IS RAW CONTENT</h2>
-                    <p>{this.props.texContent}</p>
-                    <h2>THIS IS CONVERTED CONTENT</h2>
-                    <div>
-                    <InlineTex texContent = {this.props.texContent} />
-                    </div>
-                </div>
+                <h1>INPUT QUESTION</h1>
+                <Grid container spacing={3}>
+                    <Grid item xs = {6}>
+                        <TextField multiline label = 'Input ' variant = 'outlined'
+                         fullWidth
+                         style = {{margin:8}}
+                         value = {this.props.texContent} onChange={(e)=>this.props.inputTex(e.target.value)} rowMin={3}
+                        />
+                    </Grid>
+                    <Grid item xs = {6}>
+                        <h2>THIS IS RAW CONTENT</h2>
+                        <p>{this.props.texContent}</p>
+                        <h2>THIS IS CONVERTED CONTENT</h2>
+                        <div>
+                        <InlineTex texContent = {this.props.texContent} />
+                        </div>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
