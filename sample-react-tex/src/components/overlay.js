332cc4ee22fd178a3ac6c5c307de463f51b1728c
yusuke-tanaka-ew <yusuke.tanaka.keio@gmail.com>
2021-03-20 14:36:35 +0900

    ADD OVERLAY CSS

--- sample-react-tex/src/components/overlay.js
+++ sample-react-tex/src/components/overlay.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import {Grid,TextField,Button} from '@material-ui/core'
+import '../App.css'
 
 export default class Overlay extends React.Component{
     render(){
         if(this.props.isOpenForm){
             return(
-                <Grid container spacing={1}>
-                    <Grid item xs ={1}>
+                <div className='overlay'>
+                    <div className='inputArea'>
                         <TextField multiline label = 'Input ' variant = 'outlined'
                                 fullWidth
                                 style = {{margin:8}}
                                 value = {this.props.texContent} onChange={(e)=>this.props.inputTex(e.target.value)} rowMin={3}
                                 />
-                    </Grid>
-                    <Grid item xs={1}>
                         <Button raised color='primary' onClick={()=> this.props.setOpenForm(false)}>CLOSE</Button>
-                    </Grid>
-                </Grid>
+                    </div>
+                </div>
             )
         }else{
             return null
