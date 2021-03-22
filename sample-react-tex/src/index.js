fb36051991ce6cdb88a0e50991d27f4e63d165a0
yusuke-tanaka-ew <yusuke.tanaka.keio@gmail.com>
2021-03-22 23:46:56 +0900

    ADD FIREBASE

--- sample-react-tex/src/index.js
+++ sample-react-tex/src/index.js
@@ -9,8 +9,15 @@ import TexArea from './containers/index';
 const store = createStore(texReducer)
 
 render(
-  <Provider store = {store}>
-    <TexArea />
-  </Provider>,
+  <div className='App'>
+    <div>{`REACT_APP_APIKEY:${process.env.REACT_APP_APIKEY}`}</div>
+    <div>{`REACT_APP_AUTHDOMAIN:${process.env.REACT_APP_AUTHDOMAIN}`}</div>
+    <div>{`REACT_APP_DATABASEURL:${process.env.REACT_APP_DATABASEURL}`}</div>
+    <div>{`REACT_APP_PROJECT_ID:${process.env.REACT_APP_PROJECT_ID}`}</div>
+    <div>{`REACT_APP_STORAGE_BUCKET:${process.env.REACT_APP_STORAGE_BUCKET}`}</div>
+    <div>{`REACT_APP_MESSAGING_SENDER_ID:${process.env.REACT_APP_MESSAGING_SENDER_ID}`}</div>
+    <div>{`REACT_APP_APP_ID:${process.env.REACT_APP_APP_ID}`}</div>
+    <div>{`REACT_APP_MEASUREMENT_ID:${process.env.REACT_APP_MEASUREMENT_ID}`}</div>
+  </div>,
   document.getElementById('root')
 )
